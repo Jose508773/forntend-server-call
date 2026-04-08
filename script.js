89723a24b545f73c51285fab97db84e2c458774c
Jose508773 <59071207+Jose508773@users.noreply.github.com>
2026-04-08 16:44:54 -0700

    adding front end

---
+++ script.js
@@ -0,0 +1,51 @@
+let button = document.getElementById("fetch-data");
+let showData = document.getElementById("data-container");
+
+
+
+async function getData() {
+    let response = await fetch("https://backend-server.vercel.app/api/online")
+    let data = await response.json()
+    showData.textContent = data.message
+
+}
+
+button.addEventListener("click", getData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+async function fetchData() {
+
+    let response = await fetch("/api/online", {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+
+        })
+
+
+    })
+
+
+
+
+    let data = await response.json()
+
+    showData.textContent = data.message
+
+
+
+*/
