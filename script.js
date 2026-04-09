1d0db7d974bd1e005694b5d729f9887d6f91ccf4
Jose508773 <59071207+Jose508773@users.noreply.github.com>
2026-04-08 22:22:25 -0700

    adding new api for post

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let button = document.getElementById("fetch-data");
 let showData = document.getElementById("data-container");
-
-
+let result = document.getElementById("result");
+let addNumbersButton = document.getElementById("add-numbers");
 
 async function getData() {
     let response = await fetch("https://backend-server-ruddy-ten.vercel.app/api/online")
@@ -12,40 +12,26 @@ async function getData() {
 
 button.addEventListener("click", getData)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 async function fetchData() {
+    let number1 = document.getElementById("number1")
+    let number2 = document.getElementById("number2")
+    let number1Value = number1.value
+    let number2Value = number2.value
 
-    let response = await fetch("/api/online", {
-        method: "GET",
+    let response = await fetch("https://backend-server-ruddy-ten.vercel.app/api/add", {
+        method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
-
+            a: Number(number1Value),
+            b: Number(number2Value)
         })
-
 
     })
 
-
-
-
     let data = await response.json()
+    result.textContent = data.result
 
-    showData.textContent = data.message
+}
 
+addNumbersButton.addEventListener("click", fetchData)
 
-
-*/
